767e46dd167c591666889c5573c8d95b3714bec4
Ali-Reza-001 <alirezaahmadi008hero@gmail.com>
2025-09-14 19:19:29 +0430

    new update of base url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://ali-reza.dev/api',
+  baseURL: 'https://api.ali-reza.dev/api',
 });
 
 axiosInstance.interceptors.request.use((config) => {
