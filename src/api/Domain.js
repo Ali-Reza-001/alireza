e08fe5e1278eb463b7366acd04ee0d7e29444718
Ali-Reza-001 <alirezaahmadi008hero@gmail.com>
2025-09-20 11:17:53 +0430

    nu

--- src/api/Domain.js
+++ src/api/Domain.js
@@ -1,7 +1,7 @@
 
 const DOMAIN = {
     Original: 'https://ali-reza.dev',
-    BackEnd: 'http://192.168.0.109:5000' //'https://api.ali-reza.dev',
+    BackEnd: 'https://api.ali-reza.dev',
 }
 
 export default DOMAIN;
